f032f8f540fa551110b8a7eff86101d8151fc186
adverich <facu.ft@gmail.com>
2025-03-06 13:14:54 -0300

    Preparando para la publicación

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import Button from './components/KunBtn/src/components/KunBtn.vue';
 
 export { Button };
 export default {
-  install(Vue) {
-    Vue.component('KunBtn', Button);
+  install(app) {
+    app.component('KunBtn', Button);
   },
 };
