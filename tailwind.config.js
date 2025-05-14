f77804aa1b5374fcfe9d232ee675e9c4a7e4db66
adverich <facu.ft@gmail.com>
2025-05-14 20:11:47 -0300

    ups

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ const { addDynamicIconSelectors } = require('@iconify/tailwind');
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-    content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+    content: [
+        "./index.html",
+        "./src/**/*.{vue,js,ts,jsx,tsx}",
+        "./node_modules/adverich-kun-ui/dist/**/*.{js,vue}"
+    ],
     plugins: [
         addDynamicIconSelectors(),
     ]
