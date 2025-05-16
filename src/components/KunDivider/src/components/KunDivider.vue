9a87b489c944453aa3dbbcca59c1f7eb07dfb80f
adverich <facu.ft@gmail.com>
2025-05-16 12:47:00 -0300

    ups

---
+++ src/components/KunDivider/src/components/KunDivider.vue
@@ -0,0 +1,38 @@
+<template>
+  <div
+    role="separator"
+    :class="[
+      'shrink-0',
+      vertical ? 'w-px h-full' : 'h-px w-full',
+      dashed ? 'border border-dashed border-current bg-transparent' : 'bg-current',
+      inset ? (vertical ? 'ml-4' : 'mx-4') : '',
+      color,
+    ]"
+    :style="lengthStyle"
+  />
+</template>
+
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  vertical: Boolean,
+  inset: Boolean,
+  dashed: Boolean,
+  length: [String, Number],
+  color: {
+    type: String,
+    default: ''
+  }
+})
+
+const lengthStyle = computed(() => {
+  const len = props.length != null
+    ? typeof props.length === 'number' ? `${props.length}px` : props.length
+    : undefined
+
+  return props.vertical
+    ? { height: len }
+    : { width: len }
+})
+</script>
